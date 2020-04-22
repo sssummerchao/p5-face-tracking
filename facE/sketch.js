e2376152b3d59aa794bafb07d0f9e044f3fb4548
sssummerchao <63805371+sssummerchao@users.noreply.github.com>
2020-04-22 14:16:16 -0500

    Add files via upload

--- facE/sketch.js
+++ facE/sketch.js
@@ -1,21 +1,53 @@
-function setup() {
-    loadCamera();
-    loadTracker();
-    loadCanvas(400,300);
+var ctracker;
+var video;
+var positions = [];
+var canvas;
+var emotions;
+var predictedEmotions;
+var emotionData;
 
+function setup() {
+//    loadCamera();
+//    loadCanvas(640,480);
+    canvas = createCanvas(640, 480);
+    canvas.position(0,0);
+   colorMode(HSB)
+    video = createCapture(VIDEO);
+    video.size(640, 480);
+    video.position(0, 0);
+    video.id("v");
+    var mv = document.getElementById("v");
+    mv.muted = true;
+    
+    ctracker = new clm.tracker();
+    ctracker.init(pModel);
+    ctracker.start(video.elt);
+   
+    emotions = new emotionClassifier();
+    emotions.init(emotionModel);
+    emotionData = emotions.getBlank();
 }
 
+
+
 function draw() {
-    getPositions();
-    getEmotions();
+//    getPositions();
+//    getEmotions();
 
     clear();
-
+    image(video,0,0);
     noStroke();
-    fill(0,150);
+    fill(60,150);
     rect(0,0,width,height);
-
-    drawPoints();
+   
+    positions = ctracker.getCurrentPosition();
+    var cp = ctracker.getCurrentParameters();
+    predictedEmotions = emotions.meanPredict(cp);
+    
+    fill(155);
+    for (var i=0; i<positions.length -3; i++) {
+        ellipse(positions[i][0], positions[i][1], 2, 2);
+    }
 
     if (emotions) {
         // andry=0, sad=1, surprised=2, happy=3
@@ -31,9 +63,7 @@ function draw() {
 
 }
 
-function drawPoints() {
-    fill(255);
-    for (var i=0; i<positions.length -3; i++) {
-        ellipse(positions[i][0], positions[i][1], 2, 2);
-    }
-}
+//
+//function drawPoints() {
+//
+//}
